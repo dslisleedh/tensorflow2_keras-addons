1e683ec0aff1e910c2439cf3c5e2e301eb1d233b
dslisleedh <gnova6kud@gmail.com>
2022-01-20 17:49:02 +0900

    add losses

---
+++ losses.py
@@ -0,0 +1,38 @@
+import tensorflow as tf
+
+
+class GanLoss(tf.keras.losses.Loss):
+    '''
+    fake label : 0
+    true label : 1
+    '''
+    def call(self, label, pred):
+        loss = tf.reduce_mean(
+            tf.losses.binary_crossentropy(label, pred)
+        )
+        return loss
+
+
+class LsganLoss(tf.keras.losses.Loss):
+    '''
+    fake label : 0
+    true label : 1
+    '''
+    def call(self, label, pred):
+        loss = .5 * tf.reduce_mean(
+            tf.losses.mean_squared_error(label, pred)
+        )
+        return loss
+
+
+class WganLoss(tf.keras.losses.Loss):
+    '''
+    fake label : -1
+    true label : 1
+    '''
+    def call(self, label, pred):
+        loss = tf.reduce_mean(
+            label * pred
+        )
+        return loss
+
